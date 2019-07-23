042ea47e546cf3147c25d9fc42428b408266a4a1
mattyan1053 <h.matsumaruka14161@gmail.com>
2019-07-23 15:07:14 +0900

    update for C++

--- ProCon_C/main.cpp
+++ ProCon_C/main.cpp
@@ -17,30 +17,18 @@
 
 using namespace std;
 #define endl '\n'
-#define pb push_back
+#define eb emplace_back
 #define fst first
 #define scd second
-#define EPS (1e-7)
-#define INF (1e9)
-#define PI (acos(-1))
-#define ALL(x) (x).begin(), (x).end()
-#define RALL(x) (x).rbegin(), (x).rend()
-#define REP(i,n) for(int i = 0;i < (n);i++)
-#define FOR(i,a,b) for(int i = (a);i <= (b);i++)
-#define YES(n) cout << ((n) ? "YES" : "NO" ) << endl
-#define Yes(n) cout << ((n) ? "Yes" : "No" ) << endl
 
-constexpr int MOD = 1000000007;
+constexpr int mod = 1000000007;
 
-typedef long long ll;
-typedef unsigned long long ull;
-typedef pair<int, int> P;
-typedef vector<int> vi;
-typedef vector<vector<int>> vii;
-typedef vector<ll> vl;
-
-template <class T = int> T in() { T x; cin >> x; return (x); }
-template <class T = int> void out(T x) { cout << (x) << endl; }
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using vl = vector<ll>;
 
 ll gcd(ll a, ll b) { return b ? gcd(b, a%b) : a; }
 
@@ -49,7 +37,7 @@ ll gcd(ll a, ll b) { return b ? gcd(b, a%b) : a; }
 
 void solve() {
 
-    
+
 
 	return;
 
